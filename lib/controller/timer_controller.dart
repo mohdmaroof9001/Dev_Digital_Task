d46f9b70f6fc76994fcd9b24a0fcde9dea0443a9
Mohd Maroof <mohdmaroof9001@gmail.com>
2023-08-17 16:13:12 +0530

    new changes

--- lib/controller/timer_controller.dart
+++ lib/controller/timer_controller.dart
@@ -27,7 +27,7 @@ class TimerController extends GetxController {
 
   formatSecondsToHMS(int seconds) {
     int hours = seconds ~/ 3600;
-    int minutes = (seconds % 3600) ~/ 60;
+    int minutes = seconds ~/ 60;
     int remainingSeconds = seconds % 60;
     String hoursStr = hours.toString().padLeft(2, '0');
     String minutesStr = minutes.toString().padLeft(2, '0');
